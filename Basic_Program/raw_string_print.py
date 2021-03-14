bdd582ccf084377f95a8c926e9c82fb95f045fdd
Ami Soneji <amisoneji92@gmail.com>
2021-03-14 18:50:53 +0530

    Create raw_string_print.py

---
+++ Basic_Program/raw_string_print.py
@@ -0,0 +1,5 @@
+#Author :: AMISONEJI
+#Sample Program for raw string print
+#In Python \t used for for tab and \n for new line, Python compiler consider it as tab and new line respectively. But if we want to consider \t and \n as literals we can simply write r"<string>"
+
+print(r"This for printing \t and \n as it is.") 
