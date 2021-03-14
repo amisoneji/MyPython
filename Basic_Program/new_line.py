d222b039b7be2890bf590f481fd6b3530df672fe
Ami Soneji <amisoneji92@gmail.com>
2021-03-14 18:45:30 +0530

    Create new_line.py

---
+++ Basic_Program/new_line.py
@@ -0,0 +1,4 @@
+#Author :: AMISONEJI
+#Sample Program for new line and tab
+
+print("Twinkle,twinkle,little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a dimond in the sky.\nTwinkle,twinkle,little star,\n\tHow I wonder what you are")
