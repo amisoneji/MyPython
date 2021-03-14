469cba04df436b9a325ca171ace100561dd1ecf2
Ami Soneji <amisoneji92@gmail.com>
2021-03-14 19:12:44 +0530

    Create odd_even.py

---
+++ Basic_Program/odd_even.py
@@ -0,0 +1,15 @@
+#Author :: AMISONEJI
+#Sample Program for finding odd-even numbers
+
+#Program to find out whether the entered number is even or odd.
+
+
+#user input
+num=int(input("enter number for finding odd or even:"))
+
+#if else conditions
+if num1%2==0:
+    print("Entered no is even")
+else:
+    print("Entered no is odd")
+
