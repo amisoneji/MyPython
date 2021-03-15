333dd7f6b2c193687acf04f4ee60a6228ce04787
Ami Soneji <amisoneji92@gmail.com>
2021-03-15 22:42:01 +0530

    Update replace_method.py

--- Basic_Program/String_Operations/replace_method.py
+++ Basic_Program/String_Operations/replace_method.py
@@ -1,5 +1,5 @@
 #Author :: AMISONEJI
-#Sample Program for replace method
+#Sample Programs for replace method
 """
 Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself. 
 Sample String : 'restart'
@@ -13,4 +13,24 @@ for i in s:
         s=s.replace(i,"$")
         s1=i+s[1:]
 print(s1)
+
+
+""""
+a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string. 
+Sample String : 'The lyrics is not that poor!'
+'The lyrics is poor!'
+Expected Result : 'The lyrics is good!'
+'The lyrics is poor!'
+"""
+s="The lyrics is not that poor!"
+
+snot=s.find("not")
+spoor=s.find("poor")
+
+if spoor>snot:
+    s=s.replace(s[snot:spoor+4],"good")
+    print (s)
+else:
+    print (s)
+
 
