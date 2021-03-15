d1b317efc36f7836e31898c7321c05eefb4dccd8
Ami Soneji <amisoneji92@gmail.com>
2021-03-15 22:31:26 +0530

    Create length_of_string.py

---
+++ Basic_Program/String_Operations/length_of_string.py
@@ -0,0 +1,7 @@
+#Author :: AMISONEJI
+#Sample Program for basic bank transaction calculations.
+"""
+Python program to calculate the length of a string.
+"""
+s="Here is a length of string"
+print(len(s))
