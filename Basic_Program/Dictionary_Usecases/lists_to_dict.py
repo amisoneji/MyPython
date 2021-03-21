7febd34ef1e450f65fa39a891072903b93fd52e5
Ami Soneji <amisoneji92@gmail.com>
2021-03-21 23:04:25 +0530

    Create lists_to_dict.py

---
+++ Basic_Program/Dictionary_Usecases/lists_to_dict.py
@@ -0,0 +1,11 @@
+#Author :: AMISONEJI
+#Sample Program for map lists to dict.
+
+#Python program to map two lists into a dictionary.
+
+list1=["Apple","Banana","Cat","Dog","Egg"]
+list2=["A","B","C","D","E"]
+D=dict(zip(list1,list2))
+print(D)
+
+
