ab4e8516f491d1978f29df174991a09e122dfb02
Ami Soneji <amisoneji92@gmail.com>
2021-03-21 22:37:24 +0530

    Create ascending_descending.py

---
+++ Basic_Program/Dictionary_Usecases/ascending_descending.py
@@ -0,0 +1,9 @@
+#Author :: AMISONEJI
+#Sample Program for ascennding and descending a dictionary.
+
+#Python script to sort (ascending and descending) a dictionary by key.
+
+dict={0:2,1:8,3:5,7:14,9:70}
+dict1=sorted(dict.values())
+print("ascending:",dict1)	
+print("decending:",dict1[::-1])
