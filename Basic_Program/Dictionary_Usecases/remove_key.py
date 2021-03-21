4e68836ad23955bb7c007c83e8eea475f23add71
Ami Soneji <amisoneji92@gmail.com>
2021-03-21 23:01:45 +0530

    Create remove_key.py

---
+++ Basic_Program/Dictionary_Usecases/remove_key.py
@@ -0,0 +1,11 @@
+#Author :: AMISONEJI
+#Sample Program for remove a key.
+
+
+#Python program to remove a key from a dictionary.
+D={"apple":1,"ball":2,"cat":3,"dog":4,"elephant":5}
+key=input("enter a string you want to remove")
+D.pop(key)
+print(D)
+
+
