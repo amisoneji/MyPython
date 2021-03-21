1ba12daef2db025dc7558ed5bb8f868df61e4279
Ami Soneji <amisoneji92@gmail.com>
2021-03-21 22:46:35 +0530

    Update and rename concatenatation.py to Update.py

--- Basic_Program/Dictionary_Usecases/Update.py
+++ Basic_Program/Dictionary_Usecases/Update.py
@@ -12,3 +12,11 @@ dic1.update(dic2) #all items of dict2 update in dict1
 dic1.update(dic3) #all items of dict3 update in dict1
 print(dic1)
 
+#Python script to merge two Python dictionaries.
+
+#sample dictionary
+D1={"A":"Apple","B":"Ball","C":"Cat"}
+D2={"D":"Dog","E":"Elephant","F":"Fish"}
+D=D1.copy()
+D.update(D2)
+print(D)
