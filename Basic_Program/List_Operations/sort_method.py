b992f310cb7df44a7fbf68a6c2fdc5d818c45ee6
Ami Soneji <amisoneji92@gmail.com>
2021-03-23 21:52:47 +0530

    Update sort_method.py

--- Basic_Program/List_Operations/sort_method.py
+++ Basic_Program/List_Operations/sort_method.py
@@ -9,5 +9,12 @@ list=[40,60,10,688,50,11]
 list.sort()
 print("The largest number is:",list[-1])
 
+#Python program to find the second largest number in a list. 
+
+list=[2,6,5,9,10,88,54,67]
+list.sort()
+print("second largest:",list[-2])
+
+
 
 
