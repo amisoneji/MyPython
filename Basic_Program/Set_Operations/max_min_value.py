a66d45f987bc59658f55e431c43e31f0ead10604
Ami Soneji <amisoneji92@gmail.com>
2021-03-23 22:29:34 +0530

    Create max_min_value.py

---
+++ Basic_Program/Set_Operations/max_min_value.py
@@ -0,0 +1,10 @@
+#Author :: AMISONEJI
+#Sample Program for maximum and the minimum value in a set.
+
+my_set={1,34,5,6,89,9,6,73}
+sor_set=sorted(my_set)
+print(sor_set)
+
+l1=list(sor_set)
+print ("smallest no:",l1[0])
+print ("largest no:",l1[-1])
