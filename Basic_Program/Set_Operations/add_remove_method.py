cb6d5e4043cf68a57bdb85c20bb74a89260feb3d
Ami Soneji <amisoneji92@gmail.com>
2021-03-23 22:13:57 +0530

    Create add_remove_method.py

---
+++ Basic_Program/Set_Operations/add_remove_method.py
@@ -0,0 +1,17 @@
+#Author :: AMISONEJI
+#Sample Program for remove and add element in set.
+
+#Python program to remove item(s) from set
+
+my_set={1,2,3,4,5,6}
+my_set.remove(5)
+print(my_set)
+
+#Python program to remove an item from a set if it is present in the set
+
+my_set={1,14,5,6,15,7,9}
+
+item=int(input("Enter a item you want to remove:"))
+if item in my_set:
+    my_set.remove(item)
+print(my_set)
