c3c7a463dcc0aa1f0e4826de3dd6eecbb55374b3
Ami Soneji <amisoneji92@gmail.com>
2021-03-23 21:50:39 +0530

    Create sort_method.py

---
+++ Basic_Program/List_Operations/sort_method.py
@@ -0,0 +1,13 @@
+#Author :: AMISONEJI
+#Sample Program for sort method on lists.
+
+#Python program to get the largest number from a list
+
+
+list=[40,60,10,688,50,11]
+
+list.sort()
+print("The largest number is:",list[-1])
+
+
+
