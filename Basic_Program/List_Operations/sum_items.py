b138ba065b6fa771c7316f5843216e501b48fd8b
Ami Soneji <amisoneji92@gmail.com>
2021-03-23 21:48:38 +0530

    Create sum_items.py

---
+++ Basic_Program/List_Operations/sum_items.py
@@ -0,0 +1,14 @@
+#Author :: AMISONEJI
+#Sample Program for sum of elements.
+
+#Python program to sum all the items in a list.
+
+list=[1,2,3,4,5,6,7,8,9,10]
+
+sum=0
+
+for i in list:
+    sum=sum+i
+print("sum is:",sum)
+
+
